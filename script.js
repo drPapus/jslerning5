8763c1ff2fffd7421c7c81de0174571a45320a6b
Klimov Anton <antonklimovv@gmail.com>
2015-12-10 09:10:16 +0200

    jslerning5.2

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ function maxZnach(obj){
 var a=1*obj.zn1.value;
 var b=1*obj.zn2.value;
 var c=1*obj.zn3.value;
-var m=a;
-if(b>m) m=b;
-if(c>m) m=c;
-obj.res.value=m;
+obj.res.value=Math.max(Math.max(a,b),c); 
 }
+
